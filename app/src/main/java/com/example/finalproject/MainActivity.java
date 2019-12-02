271318974e6db76f69fc422bf7cbdd7515fa47a2
isaac <isaacjs2@illinois.edu>
2019-12-02 17:48:35 -0600

    cleaned up MainActivity

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -8,63 +8,21 @@ import android.hardware.Sensor;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
-
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 
 
 public class MainActivity extends AppCompatActivity {
-
-    JsonParser jsonParser = new JsonParser();
+    private SensorManager sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+    private Sensor sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView basket = findViewById(R.id.imageView3);
+        ObjectAnimator animation = ObjectAnimator.ofFloat(basket, "translationX", 100f);
+        animation.setDuration(2000);
+        animation.start();
     }
-
-    final TextView textView = (TextView) findViewById(R.id.text);
-// ...
-
-    // Instantiate the RequestQueue.
-    /*RequestQueue queue = Volley.newRequestQueue(this);
-    String url ="http://deckofcardsapi.com";
-
-    // Request a string response from the provided URL.
-    StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-            new Response.Listener<String>() {
-                @Override
-                public void onResponse(String response) {
-                    // Display the first 500 characters of the response string.
-                    textView.setText("Response is: "+ response.substring(0,500));
-                    jsonParser.parse(stringRequest).getAsJsonObject(); //maybe?
-                }
-            }, new Response.ErrorListener() {
-        @Override
-        public void onErrorResponse(VolleyError error) {
-            textView.setText("That didn't work!");
-        }
-    });
-    // Add the request to the RequestQueue.
-    queue.add(stringRequest);
-
-     */
-
-    private SensorManager sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-    private Sensor sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
-
-    //Button button = findViewById(R.id.button);
-    //ObjectAnimator animation = ObjectAnimator.ofFloat(button, "translationX", 100f);
-
 }
-
-//idea
-//use rotation sensor for tilt controls
-//light up pieces of screen, die if user is not in area after certain time
